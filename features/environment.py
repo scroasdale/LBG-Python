812fc9385f89ba63f0caaab9a936964b0e13c9d4
scroasdale <samcroasdale@me.com>
2023-08-25 12:06:53 +0100

    added path to chromium webdriver

--- features/environment.py
+++ features/environment.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 # before all
 def before_all(context):
-    context.browser = webdriver.Chrome(r"PATH TO CHROMEDRIVER")
+    context.browser = webdriver.Chrome(r"C:\Users\Admin\Desktop\LBG-Python\webdrivers\chromedriver.exe")
     context.browser.maximize_window()
 
 def after_all(context):
